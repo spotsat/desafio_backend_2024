ac8212dcc2caa7d2ea18849a5f4caf05cde25a30
Daniel Barbosa Rocha <daniel.dr206@gmail.com>
2024-02-02 12:15:34 -0300

    update

---
+++ db/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: configService.get('NODE_ENV') === 'development',
+  logging: configService.get('NODE_ENV') === 'development',
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './module/user/user.module';
 import { GraphModule } from './module/graph/graph.module';
@@ -10,26 +10,28 @@ import { LogModule } from './module/log/log.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LogInterceptor } from './interceptors/log.interceptor';
 import * as dotenv from 'dotenv';
+import { AppConfig, DatabaseConfig } from './config';
 dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DatabaseConfig],
+    }),
     ThrottlerModule.forRoot({
       limit: 10,
       ttl: 60,
     }),
-    forwardRef(() => UserModule),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.POSTGRES_PORT, 10),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
     }),
+    forwardRef(() => UserModule),
     UserModule,
     GraphModule,
     LogModule,
